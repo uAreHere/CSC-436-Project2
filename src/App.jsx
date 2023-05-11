e4690985d9b7054a51cd63c7613997e91f05c0fa
eflores <emilio.flores@cix.csi.cuny.edu>
2023-05-10 21:17:21 -0700

    viewpost component was broken

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,9 @@ import { NavLink as RouterLink } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Container from './components/Container';
 import CreateBlog from './pages/CreateBlog';
-import ViewPost2 from './pages/ViewPost';
+import ViewPost2 from './pages/ViewPost2';
 import BlogPosts from './pages/BlogPosts';
 import EditPost from './pages/EditPost';
-//import Delete from './components/Delete';
 
 function App() {
 	return (
@@ -82,10 +81,8 @@ function App() {
 				</div>
 			</Container>
 			<Routes>
-				{/* <Route path="listall/*" element={<ListAll />} /> */}
 				<Route exact path="/" element={<BlogPosts />} />
 				<Route path="blog/:id" element={<ViewPost2 />} />
-				{/* <Route exact path="/delete" element={<Delete />} /> */}
 				<Route path="create" element={<CreateBlog />}></Route>
 				<Route path="blog/:id/edit" element={<EditPost />}></Route>
 			</Routes>
--- src/pages/ViewPost2.jsx
+++ src/pages/ViewPost2.jsx
@@ -1,25 +1,54 @@
-import { useSelector } from 'react-redux';
+import { useState, useEffect } from 'react';
+import { useParams } from 'react-router-dom';
+import axios from 'axios';
+import Container from '../components/Container';
+import Loading from '../components/Loading';
 
-const ViewPost = ({ params }) => {
-	const { postId } = params;
-	const post = useSelector((state) =>
-		state.posts.find((post) => post.id === Number(postId))
-	);
+const ViewPost = () => {
+	const { id } = useParams();
+	const [postInfo, setPostInfo] = useState();
 
-	if (!post) {
-		return <div>Post not found</div>;
-	}
+	useEffect(() => {
+		axios.get(`http://localhost:3001/v1/api/posts/${id}`).then((res) => {
+			const responsePost = res.data;
+			setPostInfo(responsePost);
+		});
+	}, []);
 
-	const { title, content, id, last_updated, originally_published } = post;
+	const {
+		id: postId,
+		title,
+		content,
+		last_updated,
+		originally_published,
+	} = postInfo || {};
 
 	return (
-		<div>
-			<h2>{title}</h2>
-			<p>{content}</p>
-			<p>ID: {id}</p>
-			<p>Last updated: {last_updated}</p>
-			<p>Originally published: {originally_published}</p>
-		</div>
+		<Container>
+			<div>
+				{postInfo ? (
+					<div className="card">
+						<div>
+							<div className="card-header">
+								<h1 className="card-title text-center"> {title} </h1>
+							</div>
+							<div className="card-body">
+								<p className="card-text">{content}</p>
+							</div>
+							<div className="card-footer text-body-secondary align-content-center">
+								<div className="d-flex inline-block">
+									<p className="m-4">Post Id: {postId}</p>
+									<p className="m-4">Created: {originally_published}</p>
+									<p className="m-4">Last Update: {last_updated}</p>
+								</div>
+							</div>
+						</div>
+					</div>
+				) : (
+					<Loading />
+				)}
+			</div>
+		</Container>
 	);
 };
 
